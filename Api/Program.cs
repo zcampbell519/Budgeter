ac700a9418e65824478307818adbed63ae1161cd
Zachary Campbell <zcampbell@outlook.com>
2023-10-12 17:53:18 -0600

    Configure the application to reference SQL Server

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,6 @@
+using Budgeter.Api.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BudgeterApiContext>(options=>{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApiConnection"));
+});
 
 var app = builder.Build();
 
